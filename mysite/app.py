92ae263894ab76e259d532575d3324a190fbe3b9
Pedrinhooo <114452531+ViviSGG@users.noreply.github.com>
2022-11-08 14:52:29 -0300

    Merge branch 'main' of https://github.com/brazadrian/Desfile-maker

--- mysite/app.py
+++ mysite/app.py
@@ -2,6 +2,26 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+@app.route('/galeria')
+def galeria():
+    return render_template("galeria.html")
+
+@app.route('/pecas')
+def pecas():
+    return render_template("pecas.html")
+
+@app.route('/cardin')
+def cardin():
+    return render_template("cardin.html")
+
+@app.route('/sobrenos')
+def sobre():
+    return render_template("sobrenos.html")
+
+@app.route('/voteaqui')
+def vote():
+    return render_template("voteaqui.html")
+
 @app.route('/')
-def principal.html():
+def principal():
     return render_template("principal.html")
